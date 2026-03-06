9e5c202e03f6eb1467627329cf736af8a7bbd6fd
Krizis112 <59326567+Krizis112@users.noreply.github.com>
2026-03-06 10:46:55 +0800

    Remove controller support and restore Simplex.noise API

--- core/src/main/java/io/w8/ucore/core/Inputs.java
+++ core/src/main/java/io/w8/ucore/core/Inputs.java
@@ -4,25 +4,15 @@ import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.InputAdapter;
 import com.badlogic.gdx.InputMultiplexer;
 import com.badlogic.gdx.InputProcessor;
-import com.badlogic.gdx.controllers.Controller;
-import com.badlogic.gdx.controllers.ControllerAdapter;
-import com.badlogic.gdx.controllers.Controllers;
-import com.badlogic.gdx.controllers.PovDirection;
 import com.badlogic.gdx.utils.Array;
 import com.badlogic.gdx.utils.IntSet;
-import io.w8.ucore.core.KeyBinds.Section;
 import io.w8.ucore.util.Input;
-import io.w8.ucore.util.Input.Type;
-import io.w8.ucore.util.Log;
-import io.w8.ucore.util.OS;
 
 public class Inputs{
-	private static boolean useControllers = true;
-	private static boolean[] buttons = new boolean[5];
-	private static IntSet keysReleased = new IntSet();
-	private static InputMultiplexer plex = new InputMultiplexer();
+	private static final boolean[] buttons = new boolean[5];
+	private static final IntSet keysReleased = new IntSet();
+	private static final InputMultiplexer plex = new InputMultiplexer();
 	private static int scroll = 0;
-	private static boolean debug = false;
 	private static InputProcessor listen = new InputAdapter(){
 		@Override
 		public boolean scrolled(int amount){
@@ -36,106 +26,25 @@ public class Inputs{
 			return false;
 		}
 	};
-	private static Array<InputDevice> devices = new Array<>();
-	
+	private static final Array<InputDevice> devices = new Array<>();
+
 	static{
 		plex.addProcessor(listen);
-		
 		devices.add(new InputDevice(DeviceType.keyboard, "Keyboard"));
-		
-		loadControllers();
 	}
 
 	public static void setDebug(boolean debug){
-		Inputs.debug = debug;
+		// controller debugging removed
 	}
 
 	public static void useControllers(boolean use){
-		useControllers = use;
-		if(!use) {
-            InputDevice keyboard = devices.get(0);
-            devices.clear();
-            devices.add(keyboard);
-        }
-	}
-
-	protected static void loadControllers(){
-		int i = 0;
-		for(Controller c : Controllers.getControllers()){
-			Inputs.getDevices().add(new InputDevice(DeviceType.controller, "Controller " + (++i), c));
-		}
-
-		Controllers.addListener(new ControllerAdapter(){
-			@Override
-			public void connected(Controller controller){
-			    if(!useControllers) return;
-				InputDevice device = new InputDevice(DeviceType.controller, "Controller " + Controllers.getControllers().size, controller);
-				Inputs.getDevices().add(device);
-			}
-
-            @Override
-            public void disconnected(Controller controller){
-                for(InputDevice d : Inputs.getDevices()){
-                    if(d.controller == controller){
-                        Inputs.getDevices().removeValue(d, true);
-                        for(Section s : KeyBinds.getSections()){
-                            if(s.device == d){
-                                s.device = devices.get(0);
-                            }
-                        }
-                        break;
-                    }
-                }
-            }
-
-			@Override
-			public boolean axisMoved(Controller controller, int axisIndex, float value) {
-				if(Math.abs(value) > 0.3f && debug){
-					Log.info("Axis: {0}, Code: {1}, Value: {2},", Input.findByType(Type.controller, axisIndex, true), axisIndex,  value);
-				}
-				return false;
-			}
-
-			@Override
-			public boolean povMoved (Controller controller, int povIndex, PovDirection value) {
-				if(debug){
-					Log.info("POV: {0}, Code: {1}, Value: {2}", Input.findByType(Type.controller, povIndex, false), povIndex,  value);
-				}
-				return false;
-			}
-
-			@Override
-			public boolean buttonDown(Controller controller, int buttonCode) {
-				if(debug) Log.info("Button: {0}, Code: {1}", Input.findByType(Type.controller, buttonCode, false),  buttonCode);
-
-				InputDevice device = findBy(controller);
-				if(device == null) return false;
-				device.pressed[buttonCode] = true;
-				return false;
-			}
-
-			@Override
-			public boolean buttonUp(Controller controller, int buttonCode) {
-				InputDevice device = findBy(controller);
-				if(device == null) return false;
-				device.released[buttonCode] = true;
-				return false;
-			}
-		});
-	}
-
-	private static InputDevice findBy(Controller controller){
-		for(InputDevice d : devices){
-			if(d.controller == controller)
-				return d;
-		}
-		return null;
+		// controller support removed
 	}
 
 	public static InputMultiplexer getProcessor(){
 		return plex;
 	}
-	
+
 	public static Array<InputDevice> getDevices(){
 		return devices;
 	}
@@ -146,56 +55,33 @@ public class Inputs{
 			buttons[i] = Gdx.input.isButtonPressed(i);
 		}
 		scroll = 0;
-		for(InputDevice device : devices){
-			if(device.type == DeviceType.keyboard) continue;
-
-			for(int i = 0; i < device.pressed.length; i ++){
-				device.pressed[i] = false;
-				device.released[i] = false;
-				device.axes[i] = device.controller.getAxis(i);
-			}
-
-			device.lastPOV = device.controller.getPov(0);
-		}
 		keysReleased.clear();
 	}
-	
+
 	public static void clearProcessors(){
 		plex.getProcessors().clear();
 	}
-	
+
 	/**Adds another input processor to the chain.*/
 	public static void addProcessor(InputProcessor listener){
 		plex.addProcessor(listener);
 		Gdx.input.setInputProcessor(plex);
 	}
-	
+
 	/**Adds another input processor to the chain at a specific index.*/
 	public static void addProcessor(int index, InputProcessor listener){
 		plex.addProcessor(index, listener);
 		Gdx.input.setInputProcessor(plex);
 	}
-	
+
 	public static void flipProcessors(){
 		plex.getProcessors().reverse();
 	}
-/*
-	public static boolean keyDown(int key){
-		return Gdx.input.isKeyPressed(key);
-	}
-	
-	public static boolean keyTap(int key){
-		return Gdx.input.isKeyJustPressed(key);
-	}
 
-	public static boolean keyRelease(int key){
-		return keysReleased.contains(key);
-	}*/
-	
 	public static boolean keyDown(String name){
 		return keyDown("default", name);
 	}
-	
+
 	public static boolean keyTap(String name){
 		return keyTap("default", name);
 	}
@@ -212,18 +98,14 @@ public class Inputs{
 		if(input == Input.UNSET)
 			return false;
 
-		if(input.type == Input.Type.controller){
-			if(input.axis) return device.controller.getAxis(input.code) > 0f;
-			if(input.pov) return device.controller.getPov(input.code) == input.direction;
-			return input.code >= 0 && device.controller.getButton(input.code);
-		}else if(input.type == Input.Type.key){
+		if(input.type == Input.Type.key){
 			return input.code >= 0 && Gdx.input.isKeyPressed(input.code);
 		}else if(input.type == Input.Type.mouse){
 			return Gdx.input.isButtonPressed(input.code);
 		}
 		return false;
 	}
-	
+
 	public static boolean keyDown(String section, String name){
 		KeyBinds.Section s = KeyBinds.getSection(section);
 		if(KeyBinds.has(section, name)){
@@ -243,18 +125,14 @@ public class Inputs{
 		if(input == Input.UNSET)
 			return false;
 
-		if(input.type == Input.Type.controller){
-            if(input.axis) return device.controller.getAxis(input.code) > 0f && device.axes[input.code] < 0;
-			if(input.pov) return device.controller.getPov(input.code) == input.direction && device.lastPOV != input.direction;
-			return input.code >= 0 && device.pressed[input.code];
-		}else if(input.type == Input.Type.key){
+		if(input.type == Input.Type.key){
 			return Gdx.input.isKeyJustPressed(input.code);
 		}else if(input.type == Input.Type.mouse){
 			return Gdx.input.isButtonPressed(input.code) && !buttons[input.code];
 		}
 		return false;
 	}
-	
+
 	public static boolean keyTap(String section, String name){
 		KeyBinds.Section s = KeyBinds.getSection(section);
 		if(KeyBinds.has(section, name)){
@@ -274,10 +152,7 @@ public class Inputs{
 		if(input == Input.UNSET)
 			return false;
 
-		if(input.type == Input.Type.controller){
-			if(input.pov) return device.controller.getPov(input.code) != input.direction && device.lastPOV == input.direction;
-			return input.code >= 0 && device.released[input.code];
-		}else if(input.type == Input.Type.key){
+		if(input.type == Input.Type.key){
 			return keysReleased.contains(input.code);
 		}else if(input.type == Input.Type.mouse){
 			return !Gdx.input.isButtonPressed(input.code) && buttons[input.code];
@@ -307,116 +182,72 @@ public class Inputs{
 		KeyBinds.Section s = KeyBinds.getSection(section);
 		Axis axis = KeyBinds.getAxis(section, name);
 
-		if(s.device.type == DeviceType.controller){
-			Controller c = s.device.controller;
-
-			if(axis.min.axis){
-				return c.getAxis(axis.min.code) * (axis.min.name().contains("VERTICAL") && !OS.isWindows ? -1 : 1);
-			}else{
-				boolean min = axis.min.pov ? c.getPov(0) == axis.min.direction : c.getButton(axis.min.code),
-						max = axis.max.pov ? c.getPov(0) == axis.max.direction : c.getButton(axis.max.code);
-				return (min && max) || (!min && !max) ? 0 : min ? -1 : 1;
-			}
-		}else{
-			if(axis.min == Input.SCROLL){
-				return scroll();
-			}else {
-				boolean min = keyDown(axis.min, s.device), max = keyDown(axis.max, s.device);
-				return (min && max) || (!min && !max) ? 0 : min ? -1 : 1;
-			}
+		if(axis.min == Input.SCROLL){
+			return scroll();
 		}
+
+		boolean min = keyDown(axis.min, s.device);
+		boolean max = keyDown(axis.max, s.device);
+		return (min && max) || (!min && !max) ? 0 : min ? -1 : 1;
 	}
 
 	public static InputDevice getKeyboard(){
 		return devices.get(0);
 	}
-	
+
 	public static boolean buttonDown(int button){
 		return Gdx.input.isButtonPressed(button);
 	}
-	
+
 	public static boolean buttonUp(int button){
 		return Gdx.input.isButtonPressed(button) && !buttons[button];
 	}
-	
+
 	public static boolean buttonRelease(int button){
 		return !Gdx.input.isButtonPressed(button) && buttons[button];
 	}
-	
+
 	public static int scroll(){
 		return scroll;
 	}
-	
+
 	public static boolean scrolled(){
 		return Math.abs(scroll) > 0;
 	}
-	
+
 	static void dispose(){
 		plex.getProcessors().clear();
 	}
 
-	public static ControllerType getControllerType(Controller controller){
-		String check = controller.getName().toLowerCase();
-		if(check.contains("x-box") || check.contains("xbox")){
-			return ControllerType.xbox;
-		}else{
-			return ControllerType.unknown;
-		}
-	}
-
-	/**Represents either a keyboard or controller.*/
+	/**Represents an input device (keyboard only in this build).*/
 	public static class InputDevice{
-		public static final int BUTTONS = 32;
-
 		public final DeviceType type;
 		public final String name;
-		public final Controller controller;
-		public final ControllerType controllerType;
-		public final boolean[] pressed = new boolean[BUTTONS];
-		public final boolean[] released = new boolean[BUTTONS];
-		public final float[] axes = new float[BUTTONS];
-		public PovDirection lastPOV = PovDirection.center;
-		
+
 		public InputDevice(DeviceType type, String name){
-			this(type, name, null);
-		}
-		
-		public InputDevice(DeviceType type, String name, Controller controller){
 			this.type = type;
 			this.name = name;
-			this.controller = controller;
-			if(controller != null){
-				controllerType = getControllerType(controller);
-			}else{
-				controllerType = null;
-			}
 		}
 	}
 
 	//TODO 2D axes, like controller sticks, as having two different axes is confusing.
-	//TODO don't treat triggers as axes?
-	/**Represents an input axis. When using a mouse or keyboard, both values are used;
-	 * When a controller is used, min is the only key checked, as it is already an axis.*/
+	/**Represents an input axis.*/
 	public static class Axis{
 		public Input min, max;
 
-		/**Cosntructor for axes only.*/
+		/**Constructor for axis-only inputs.*/
 		public Axis(Input axis){
 			min = max = axis;
 		}
 
-		/**Constructor for keyboards, or multiple buttons on a controller.*/
+		/**Constructor for dual-direction inputs.*/
 		public Axis(Input min, Input max){
 			this.min = min;
 			this.max = max;
 		}
 	}
-	
-	public enum DeviceType{
-		keyboard, controller
-	}
 
-	public enum ControllerType{
-		xbox, unknown;
+	public enum DeviceType{
+		keyboard
 	}
 }
